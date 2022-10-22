ab99c8b9e4acc8457b66635b1a8946eeeb7b5ae0
Chris <chris.barwick16@gmail.com>
2022-10-21 18:06:21 -0700

    Config v0.3r2
    - Add `ally` tag to swap tile

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ local Settings = require(game.ServerStorage.Services.SettingsService):GetSetting
 local BoardController = require(game.ServerStorage.Controllers.BoardController)
 
 return {
-	"0.3r1",
+	"0.3r2",
 	{
 		name = "knight",
 		enabled = true,
@@ -85,7 +85,7 @@ return {
 		enabled = true,
 		icon = "rbxassetid://6176342066",
 		max = 6,
-		tags = Set({"no-swap", "no-spawn"}),
+		tags = Set({"no-swap", "no-spawn", "ally"}),
 		entityType = "Tile",
 		getValidMoves = function(selfTile: Types.Tile): {Types.Tile}
 			return TileRuleManager.getValidForTile(selfTile)
